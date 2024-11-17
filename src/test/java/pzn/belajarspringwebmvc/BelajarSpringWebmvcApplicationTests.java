427f7ceab57db40f4aa17a70f6be16968d94112a
arfinorestualam <arfino.alam@gmail.com>
2024-11-18 01:57:24 +0700

    first

---
+++ src/test/java/pzn/belajarspringwebmvc/BelajarSpringWebmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package pzn.belajarspringwebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringWebmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
