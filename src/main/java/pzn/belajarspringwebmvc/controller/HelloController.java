d6602914fa7e6300389d382d40dd08e013541523
arfinorestualam <arfino.alam@gmail.com>
2024-11-19 05:18:18 +0700

    make request mapping

--- src/main/java/pzn/belajarspringwebmvc/controller/HelloController.java
+++ src/main/java/pzn/belajarspringwebmvc/controller/HelloController.java
@@ -1,7 +1,17 @@
 package pzn.belajarspringwebmvc.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 @Controller
 public class HelloController {
+    //menambahkan request mapping (controller handler?)
+    @RequestMapping(path= "/hello")
+    public void helloWorld(HttpServletResponse response) throws IOException {
+        response.getWriter().println("Hello World");
+    }
+    //bisa membuat banyak controller handler, tidak cuma 1
 }
