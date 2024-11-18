ae76291c552af98ad15f7979dcd67466e117fd0c
arfinorestualam <arfino.alam@gmail.com>
2024-11-19 05:42:01 +0700

    unit test using mock mvc

---
+++ src/test/java/pzn/belajarspringwebmvc/controller/HelloControllerTest.java
@@ -0,0 +1,45 @@
+package pzn.belajarspringwebmvc.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+//membuat mock mvc, unit test yang tidak perlu lagi kita mengetes di browser
+//testing semua controller yang ada menggunakan mock mvc
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    //menggunakan mockMvc untuk get request helloGuest() dan helloName()
+
+    @Test
+    void helloGuest() throws Exception {
+        mockMvc.perform(get("/hello")
+        ).andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("Hello Guest!"))
+        );
+    }
+
+    @Test
+    void helloName() throws Exception {
+        mockMvc.perform(
+                get("/hello").queryParam("name", "fin")
+        ).andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("Hello fin!"))
+        );
+    }
+}
