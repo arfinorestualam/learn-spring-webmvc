accf0b78f9e0729d12e81bf2d93da3505b0bb514
arfinorestualam <arfino.alam@gmail.com>
2024-11-19 05:24:14 +0700

    running web

--- src/main/java/pzn/belajarspringwebmvc/BelajarSpringWebmvcApplication.java
+++ src/main/java/pzn/belajarspringwebmvc/BelajarSpringWebmvcApplication.java
@@ -3,6 +3,9 @@ package pzn.belajarspringwebmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//tinggal klik run saja disini untuk menjalankan webnya
+//buka browser, karena kita menggunakan local host, tinggal ke
+// localhost:8181/hello, karena kita belom handle untuk localhost:8181
 @SpringBootApplication
 public class BelajarSpringWebmvcApplication {
 
