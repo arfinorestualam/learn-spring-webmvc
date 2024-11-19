7a9eac4468028b3a0fc6e5c997e273e036384537
arfinorestualam <arfino.alam@gmail.com>
2024-11-20 04:27:57 +0700

    learn response content type

--- src/test/java/pzn/belajarspringwebmvc/controller/FormControllerMockTest.java
+++ src/test/java/pzn/belajarspringwebmvc/controller/FormControllerMockTest.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,6 +27,8 @@ class FormControllerMockTest {
                         .param("name", "fin")
         ).andExpectAll(
                 status().isOk(),
+                //menambahkan header untuk mengetahui bener ngga si responsenya sesuai dengan yang kita inginkan
+                header().string(HttpHeaders.CONTENT_TYPE, Matchers.containsString(MediaType.TEXT_HTML_VALUE)),
                 content().string(Matchers.containsString("Hello fin"))
         );
     }
