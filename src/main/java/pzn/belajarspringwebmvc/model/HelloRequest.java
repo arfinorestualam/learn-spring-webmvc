5c145be6fd97a92da6b17ad9587946d4735c090a
arfinorestualam <arfino.alam@gmail.com>
2024-11-22 15:52:21 +0700

    learn request body

---
+++ src/main/java/pzn/belajarspringwebmvc/model/HelloRequest.java
@@ -0,0 +1,13 @@
+package pzn.belajarspringwebmvc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloRequest {
+    private String name;
+
+}
