d2544eed1cefd018387ddaf05eadc116016d208e
arfinorestualam <arfino.alam@gmail.com>
2024-11-23 03:30:38 +0700

    learn how to make response and request json

---
+++ src/main/java/pzn/belajarspringwebmvc/controller/ApiController.java
@@ -0,0 +1,24 @@
+package pzn.belajarspringwebmvc.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pzn.belajarspringwebmvc.model.CreatePersonRequest;
+
+@Controller
+public class ApiController {
+
+    @PostMapping(
+            path = "/api/person",
+            //cause we set up the consumes and produces using json value
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    //then in here, we dont need String as Response, we can just set it to the model
+    public CreatePersonRequest createPerson(@RequestBody CreatePersonRequest request) {
+        return request;
+    }
+}
---
+++ src/test/java/pzn/belajarspringwebmvc/controller/ApiControllerMockTest.java
@@ -0,0 +1,62 @@
+package pzn.belajarspringwebmvc.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import pzn.belajarspringwebmvc.model.CreateAddressRequest;
+import pzn.belajarspringwebmvc.model.CreatePersonRequest;
+import pzn.belajarspringwebmvc.model.CreateSocialMediaRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ApiControllerMockTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void createPerson() throws Exception {
+        CreatePersonRequest request = new CreatePersonRequest();
+        request.setFirstName("Test");
+        request.setMiddleName("Test");
+        request.setLastName("Test");
+        request.setEmail("test@test.com");
+        request.setPhoneNumber("1234567890");
+        request.setAddress(new CreateAddressRequest());
+        request.getAddress().setCity("Test");
+        request.getAddress().setCountry("Test");
+        request.getAddress().setPostalCode("Test");
+        request.getAddress().setStreet("Test");
+        request.setHobbies(List.of("code", "read"));
+        request.setSocialMedias(new ArrayList<>());
+        request.getSocialMedias().add(new CreateSocialMediaRequest("Facebook", "fb.me/"));
+        request.getSocialMedias().add(new CreateSocialMediaRequest("Instagram", "instagram.me/"));
+
+        String jsonRequest = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(
+                post("/api/person")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest)
+        ).andExpectAll(
+                status().isOk(),
+                //for check if the json same
+                content().json(jsonRequest)
+        );
+    }
+}
