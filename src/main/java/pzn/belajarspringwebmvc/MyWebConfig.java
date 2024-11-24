33a0cdd86266ee881aa4dbbb277fe0432e316441
arfinorestualam <arfino.alam@gmail.com>
2024-11-24 09:18:06 +0700

    learn mvc config

---
+++ src/main/java/pzn/belajarspringwebmvc/MyWebConfig.java
@@ -0,0 +1,12 @@
+package pzn.belajarspringwebmvc;
+
+//make configuration for web mvc
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+//must implement web mvc configurer
+public class MyWebConfig implements WebMvcConfigurer {
+
+}
